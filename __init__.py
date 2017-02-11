86b8dad4455685b3aaf0bd7908132a8888c2a036
frreinert <frreinert@frreinert-1513>
2017-02-11 19:04:53 -0200

    New Features
    - Added log system
    - Translated to EN
    - Log the processing time
    - Added a "try" on renaming part to prevent failures (need to be tested)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 import sys
 import os
 import uuid
+import timeit
+import logging as log
+from time import gmtime, strftime
+
+# Log file setup
+log.basicConfig(filename='maxlength-folder-eraser.log', level=log.INFO)
 
 class output():
     
@@ -18,40 +24,40 @@ class version():
     Version information
     """
     VERS        = 0.1
-    DAT         = "20/01/2017"
-    COPYRIGHT   = "Desenvolvido por Fabricio Roberto Reinert"
+    DAT         = "11/02/2017"
+    COPYRIGHT   = "Developed by Fabricio Roberto Reinert"
     
     def __init__(self, silent):
         o = output(silent)
-        o.out("-----------------------------------------")
-        o.out("Versão:         %f" % self.VERS)
-        o.out("Ultima revisão: %s" % self.DAT)
+        o.out("-------------------------------------")
+        o.out("Version:         %f" % self.VERS)
+        o.out("Last Revision:   %s" % self.DAT)
         o.out(self.COPYRIGHT)
-        o.out("-----------------------------------------")
+        o.out("-------------------------------------")
         
 
 class Argumentos():
     """
-    Manipulação dos argumentos iniciais
+    Handle initial arguments
     """
     params  = dict()
     
     # handle the arguments and insert them into the dictionary "params"
     def __init__(self):
         if len(sys.argv) == 1:
-            sys.exit("\n\n        Limpeza de Backups\n\n    -dir : Especifíca um diretório\n    -silent : Modo silencioso\n\n")
+            sys.exit("\n\n        Folder Name Shortener Tool\n\n    -dir : Specify a folder\n    -silent : Silent mode\n\n")
         
         for p in sys.argv:
             
             if p == "-dir":
                 dir_index = sys.argv.index("-dir",)
                 if dir_index+1 > len(sys.argv)-1:
-                    sys.exit("\n>> ERRO: Diretório não especificado")
+                    sys.exit("\n>> ERROR: Folder not specified")
                 else:
                     dir_index = dir_index+1
                     self.params["DIR"] = sys.argv[dir_index]
                     if not os.path.isdir(self.params["DIR"]):
-                        sys.exit("\n>> ERRO: Diretório '%s' não existe" % self.params["DIR"])
+                        sys.exit("\n>> ERROR: Folder  '%s' do not exist" % self.params["DIR"])
             
             if p == "-silent":
                 self.params["SILENT"] = True
@@ -61,16 +67,15 @@ class Argumentos():
     
     # Print Arguments
     def PrintArgs(self): 
-        print("\n\n-----------\nArgumentos:\n-----------")
+        print("\n\n-----------\nArguments:\n-----------")
         for k, v in self.params.items():
             print("+ %s: %s" % (k,v))
             
-def Analise(folders, silent=True):
+def Analise(folders):
     
     # Dirs receives subfolders
     Dirs    = []
-    o       = output(silent)    
-    
+     
     # walk trough the folders on the "folders" param
     for folder in folders:
         
@@ -82,9 +87,16 @@ def Analise(folders, silent=True):
             UID = uuid.uuid4()
             UID = UID.hex [:8]
             
-            os.rename(os.path.join(folder,d), os.path.join(folder, UID))
-            
-            o.out('alterado: ' + os.path.join(folder,d) + ' para ' + os.path.join(folder, UID))
+            # Log the entire process to a file
+            try:
+                os.rename(os.path.join(folder,d), os.path.join(folder, UID))
+                log.info('Processing: ' + os.path.join(folder,d) + ' updated to: ' + os.path.join(folder, UID))
+            except:
+                
+                e = sys.exc_info()[0]
+                
+                log.warning(os.path.join(folder,d) + ' could not be renamed to: ' + os.path.join(folder, UID))
+                log.error(e)
             
             
         # Read again the dirs - now it's already renamed
@@ -99,10 +111,26 @@ def Analise(folders, silent=True):
             
             
         # Recursively calls the function passing the list "Dirs" as a new parameter
-        Analise(Dirs, silent)   
+        Analise(Dirs)   
         
         
-        
+class Timer:
+    """
+    Used to calculate time spent
+    and others time stuff 
+    """
+    
+    time_start  = 0
+    time_end    = 0
+    
+    def __init__(self):
+        self.time_start = timeit.default_timer()
+        log.info("Process started at %s" % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+                     
+    def stop(self):
+        self.time_end = timeit.default_timer()
+        log.info("Process finished at %s" % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+        log.info("Time elapsed in this process is %s seconds" % str(self.time_end - self.time_start))
 
 # Get the arguments
 _args = Argumentos()
@@ -118,8 +146,16 @@ _ver = version(SILENT)
 if not SILENT:
     _args.PrintArgs()
 
+
+# Start the process log
+log.info("Target folder is: %s" % os.path.abspath(DIR[0]))
+Time = Timer()
+
 # Call the main function to rename files and folders 
-_analyzer = Analise(DIR, SILENT)
+_analyzer = Analise(DIR)
+
+# Finish process
+Time.stop()
 
 
 
